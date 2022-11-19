91e0e665d8684e313df1992e1394da9474c92dab
g-80 <ghaith.ger@gmail.com>
2022-11-19 20:51:42 +0000

    added . to imports

--- things/forms.py
+++ things/forms.py
@@ -1,6 +1,6 @@
 """Forms of the project."""
 from django.forms import ModelForm, Textarea
-from models import Thing
+from .models import Thing
 # Create your forms here.
 
 class ThingForm(ModelForm):
